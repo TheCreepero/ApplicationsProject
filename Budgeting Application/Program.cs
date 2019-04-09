1dd6d3e446a917e34cb428fdb36dfbd63ba66c51
Eetu Palo <eetu.palo@gmail.com>
2019-04-09 18:00:35 +0300

    The application is now able to list users from the database! (I had to convert the DB to a MDF file.)

--- Budgeting Application/Program.cs
+++ Budgeting Application/Program.cs
@@ -12,6 +12,9 @@ namespace Budgeting_Application
 {
     class Program:System.Windows.Forms.Form
     {
+        private ListBox userList;
+        private Button selectUser;
+
         Program()
         {
             InitializeComponent();
@@ -21,32 +24,74 @@ namespace Budgeting_Application
         {
             Application.EnableVisualStyles();
             Application.Run(new Program());
-            /*
-            DbConnection startup = new DbConnection();
-            startup.OpenConnection();
 
-            string listUsers = "SELECT UserName, UserID FROM [User]";
-
-            startup.DataReader(listUsers);
-
-            DataTable users = new DataTable();
-
-            startup.ShowDataInGridView(listUsers);
-
-            startup.CloseConnection();
-            */
         }
 
         private void InitializeComponent()
         {
+            this.userList = new System.Windows.Forms.ListBox();
+            this.selectUser = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // userList
+            // 
+            this.userList.FormattingEnabled = true;
+            this.userList.Location = new System.Drawing.Point(12, 12);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(311, 121);
+            this.userList.TabIndex = 0;
+            this.userList.SelectedIndexChanged += new System.EventHandler(this.userList_SelectedIndexChanged);
+            // 
+            // selectUser
+            // 
+            this.selectUser.Location = new System.Drawing.Point(12, 150);
+            this.selectUser.Name = "selectUser";
+            this.selectUser.Size = new System.Drawing.Size(157, 61);
+            this.selectUser.TabIndex = 1;
+            this.selectUser.Text = "Select User";
+            this.selectUser.UseVisualStyleBackColor = true;
+            this.selectUser.Click += new System.EventHandler(this.selectUser_Click);
             // 
             // Program
             // 
             this.ClientSize = new System.Drawing.Size(335, 444);
+            this.Controls.Add(this.selectUser);
+            this.Controls.Add(this.userList);
             this.Name = "Program";
             this.ResumeLayout(false);
 
+        }
+
+        private void userList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void selectUser_Click(object sender, EventArgs e)
+        {
+            DbConnection listUsersButton = new DbConnection();
+            string listUsers = "SELECT UserName FROM [User]";
+
+            try
+            {
+                listUsersButton.OpenConnection();
+
+                SqlDataReader dr = listUsersButton.DataReader(listUsers);
+
+                while (dr.Read())
+                {
+                    userList.Items.Add(dr["UserName"].ToString());
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                listUsersButton.CloseConnection();
+            }
         }
     }
 }
