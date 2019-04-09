551011679ffb465b3b2f68035b6cbd92a80e6d37
Eetu Palo <eetu.palo@gmail.com>
2019-04-09 21:05:16 +0300

    -

--- Budgeting Application/Program.cs
+++ Budgeting Application/Program.cs
@@ -13,7 +13,12 @@ namespace Budgeting_Application
     class Program:System.Windows.Forms.Form
     {
         private ListBox userList;
-        private Button selectUser;
+        private Button button1;
+        private Button exit;
+        private Label label1;
+        private Button getUserList;
+
+        public static string selectedUser;
 
         Program()
         {
@@ -30,47 +35,85 @@ namespace Budgeting_Application
         private void InitializeComponent()
         {
             this.userList = new System.Windows.Forms.ListBox();
-            this.selectUser = new System.Windows.Forms.Button();
+            this.getUserList = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // userList
             // 
             this.userList.FormattingEnabled = true;
-            this.userList.Location = new System.Drawing.Point(12, 12);
+            this.userList.Location = new System.Drawing.Point(12, 39);
             this.userList.Name = "userList";
-            this.userList.Size = new System.Drawing.Size(311, 121);
+            this.userList.Size = new System.Drawing.Size(160, 121);
             this.userList.TabIndex = 0;
             this.userList.SelectedIndexChanged += new System.EventHandler(this.userList_SelectedIndexChanged);
             // 
-            // selectUser
+            // getUserList
             // 
-            this.selectUser.Location = new System.Drawing.Point(12, 150);
-            this.selectUser.Name = "selectUser";
-            this.selectUser.Size = new System.Drawing.Size(157, 61);
-            this.selectUser.TabIndex = 1;
-            this.selectUser.Text = "Select User";
-            this.selectUser.UseVisualStyleBackColor = true;
-            this.selectUser.Click += new System.EventHandler(this.selectUser_Click);
+            this.getUserList.Location = new System.Drawing.Point(12, 166);
+            this.getUserList.Name = "getUserList";
+            this.getUserList.Size = new System.Drawing.Size(160, 27);
+            this.getUserList.TabIndex = 1;
+            this.getUserList.Text = "List Users";
+            this.getUserList.UseVisualStyleBackColor = true;
+            this.getUserList.Click += new System.EventHandler(this.selectUser_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open Application";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(12, 232);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(160, 27);
+            this.exit.TabIndex = 3;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select the user from the list:";
             // 
             // Program
             // 
-            this.ClientSize = new System.Drawing.Size(335, 444);
-            this.Controls.Add(this.selectUser);
+            this.ClientSize = new System.Drawing.Size(184, 269);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.getUserList);
             this.Controls.Add(this.userList);
             this.Name = "Program";
+            this.Load += new System.EventHandler(this.Program_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         private void userList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            selectedUser = userList.SelectedItem.ToString();
         }
 
         private void selectUser_Click(object sender, EventArgs e)
         {
             DbConnection listUsersButton = new DbConnection();
-            string listUsers = "SELECT UserName FROM [User]";
+            string listUsers = "SELECT UserID, UserName, UserLvl FROM [User]";
 
             try
             {
@@ -92,6 +135,22 @@ namespace Budgeting_Application
             {
                 listUsersButton.CloseConnection();
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form mainmenu = new Form();
+            mainmenu.Show();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Program_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Budgeting Application/mainmenu.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Budgeting_Application
+{
+    partial class mainmenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Location = new System.Drawing.Point(13, 13);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(253, 13);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "string label = \"Hello, \" + Program.selectedUser + \".\";";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // mainmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.welcomeLabel);
+            this.Name = "mainmenu";
+            this.Text = "mainapp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+
+        //private System.Windows.Forms.Label label1;
+    }
+}
